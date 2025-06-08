c8be8713a122f21beb3b169b95c6826581c18a73
Mikhail Chernetsov <chernetsov@gmail.com>
2025-06-08 13:43:01 -0500

    crisper favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,8 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon-48x48.png" sizes="48x48" type="image/png" />
+        <link rel="icon" href="/favicon-96x96.png" sizes="96x96" type="image/png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="4b8195ad-8e01-4b8c-87c5-852a7ebd4ec6"></script>
       </head>
